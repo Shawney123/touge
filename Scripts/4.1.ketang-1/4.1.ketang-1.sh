58607f6d789215cd44247519762638bd1bec58a9
Shawney123 <8002124110@email.ncu.edu.cn>
2026-05-07 13:01:39 +0800

    second commit

---
+++ Scripts/4.1.ketang-1/4.1.ketang-1.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+DownloadSite=${1:-Gitee}
+
+mkdir -p /tmp/4.1.ketang-1
+case $DownloadSite in
+    Github)
+        wget -O /tmp/4.1.ketang-1/第一关.txt https://raw.githubusercontent.com/Shawney123/touge/main/Source/4.1.ketang-1/%E7%AC%AC%E4%B8%80%E5%85%B3.txt
+        wget -O /tmp/4.1.ketang-1/gdb.log  https://raw.githubusercontent.com/Shawney123/touge/main/Source/4.1.ketang-1/gdb.log
+        ;;
+    Gitee)
+        wget -O /tmp/4.1.ketang-1/第一关.txt https://gitee.com/shawney123/touge/raw/main/Source/4.1.ketang-1/%E7%AC%AC%E4%B8%80%E5%85%B3.txt
+        wget -O /tmp/4.1.ketang-1/gdb.log https://gitee.com/shawney123/touge/raw/main/Source/4.1.ketang-1/gdb.log
+        ;;
+    *)
+        echo "Unknown download site specified: $DownloadSite"
+        echo "未知下载站点: $DownloadSite"
+        exit 1
+        ;;
+esac
+mv -f /tmp/4.1.ketang-1/第一关.txt /data/workspace/myshixun/第一关.txt
+mv -f /tmp/4.1.ketang-1/gdb.log /root/gdb.log
+
+exit 0
